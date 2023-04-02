c04842fabb0287ebfdec7165d82709037bf121d9
Yirmel-Sanchez <yirmelaugusto.sanchez@alu.uclm.es>
2023-04-02 19:28:37 +0200

    backend usuarios

---
+++ src/main/java/edu/uclm/esi/gamesusers/GamesUsersApplication.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.gamesusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uclm/esi/gamesusers/GamesUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.gamesusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
